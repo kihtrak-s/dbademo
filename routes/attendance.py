83f67a37fd1bb99a3aaddc3a9e8fee7cac54d8a9
karthik <Karthik.Srinivasan@efi.com>
2025-05-05 00:11:10 +0530

    Updated attendance table

--- routes/attendance.py
+++ routes/attendance.py
@@ -7,7 +7,7 @@ from sql import fetch_all_attendance
 attendance_bp = Blueprint('attendence',__name__)
 
 table_name='Student'
-table_attributes=['StudentID','Name','DOB','Gender','AdmissionNo','ClassID','SectionID','ParentInfo']
+table_attributes=['attendanceID', 'date', 'studentID', 'status']
 attendance_api='/attendance'
 
 @attendance_bp.route(attendance_api,methods=['GET'])
